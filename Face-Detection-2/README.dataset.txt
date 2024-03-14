ee38ce922567961980ae2997a771317802da6564
ThesusCaryed <thesis.cgr@gmail.com>
2024-03-14 17:06:55 +0800

    mar14

---
+++ Face-Detection-2/README.dataset.txt
@@ -0,0 +1,6 @@
+# Face-Detection > DatasetV2
+https://universe.roboflow.com/thesis-cg8kk/face-detection-mgde2
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- download_dataset.py
+++ download_dataset.py
@@ -21,27 +21,32 @@ from IPython.display import Image
 print('Setup complete. Using torch %s %s' % (torch.__version__, torch.cuda.get_device_properties(0) if torch.cuda.is_available() else 'CPU'))
 
 # Set up environment
-os.environ["DATASET_DIRECTORY"] = r"C:\Users\Acer\Desktop\ThesisV3\ThesisV3"
+os.environ["DATASET_DIRECTORY"] = r"../"
 
 # Initialize Roboflow
 # rf = Roboflow(model_format="yolov5", api_key="AVzps2ggtt5RXGeBEglG")
 # project = rf.workspace("thesis-cg8kk").project("face-detection-cr55d")
 # dataset = project.version(1).download("yolov5")
 
-from roboflow import Roboflow
 rf = Roboflow(api_key="AVzps2ggtt5RXGeBEglG")
 project = rf.workspace("thesis-cg8kk").project("face-detection-mgde2")
-version = project.version(1)
+version = project.version(2)
 dataset = version.download("yolov5")
+
+#from roboflow import Roboflow
+#rf = Roboflow(api_key="AVzps2ggtt5RXGeBEglG")
+#project = rf.workspace("thesis-cg8kk").project("face-detection-mgde2")
+#version = project.version(1)
+#dataset = version.download("yolov5")
 
 
 # Train YOLOv5 model
-#subprocess.run(["python", "train.py", "--img", "416", "--batch", "16", "--epochs", "20", "--data", f"{dataset.location}/data.yaml", "--weights", "yolov5s.pt", "--cache"])
+subprocess.run(["python", "train.py", "--img", "416", "--batch", "16", "--epochs", "20", "--data", f"{dataset.location}/data.yaml", "--weights", "yolov5s.pt", "--cache"])
 
 # Display training results
-#from utils.plots import plot_results  # Plot results.txt as results.png
-#Image(filename='runs/train/exp/results.png', width=1000)  # View results.png
+from utils.plots import plot_results  # Plot results.txt as results.png
+Image(filename='runs/train/exp/results.png', width=1000)  # View results.png
 
 # Display ground truth training data
-#print("GROUND TRUTH TRAINING DATA:")
-#Image(filename='runs/train/exp/val_batch0_labels.jpg', width=900)
+print("GROUND TRUTH TRAINING DATA:")
+Image(filename='runs/train/exp/val_batch0_labels.jpg', width=900)
